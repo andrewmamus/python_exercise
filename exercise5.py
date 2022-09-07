8e46f2cc92118a2575d6bc9cdd1a5d1df117ea15
Akpeghughu Andrew Mamus <muyontech@gmail.com>
2022-09-07 23:37:52 +0100

    Exercise 5 solution

---
+++ exercise5.py
@@ -0,0 +1,9 @@
+# Write a program that reads two integer numbers and print the maximum number among them.
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 > num2:
+    print(num1, " is the greatest number")
+else:
+     print(num2, " is the greatest number")
